43676e49641ecd68224794466bf6579f69fa955b
Omkar M Javali <omkarjavali11@gmail.com>
2025-05-04 10:51:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Use a faster or more reliable Debian mirror
-echo "Using deb.debian.org mirror"
-sudo sed -i 's|http://deb.debian.org/debian|http://ftp.us.debian.org/debian|g' /etc/apt/sources.list
+# Use faster mirror (optional)
+sudo sed -i 's|http://deb.debian.org|http://ftp.us.debian.org|g' /etc/apt/sources.list
 
-# Update package list
-sudo apt-get update -y
+# Retry update and install logic
+RETRIES=5
+COUNT=0
+until [ $COUNT -ge $RETRIES ]
+do
+  sudo apt-get update -y && sudo apt-get install -y libgl1-mesa-glx libglib2.0-0 && break
+  COUNT=$((COUNT+1))
+  echo "Retrying apt install... attempt $COUNT"
+  sleep 5
+done
